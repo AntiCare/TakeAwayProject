8443b2bffe4586b81861408a23c614fbf2f319a1
yang Cheng <474340@student.saxion.nl>
2020-09-24 02:50:22 +0200

    - finish search function

--- TestNav/app/src/main/java/nl/saxion/testnav/fragments/home/HomeFragment.java
+++ TestNav/app/src/main/java/nl/saxion/testnav/fragments/home/HomeFragment.java
@@ -2,11 +2,14 @@ package nl.saxion.testnav.fragments.home;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,10 +21,11 @@ import nl.saxion.testnav.R;
 import nl.saxion.testnav.RestaurantItemActivity;
 
 public class HomeFragment extends Fragment {
-
     private HomeViewModel homeViewModel;
     private ListView listView;
-
+    private SearchView mSearchView;
+private String[] restaurantName = { "Burger king", "Pizza king", "Sushi king", "BBQ king", "Kapsalon king","Doner king","Fries king","Pasta king","Fish king","Chicken king"
+        ,"Seafood king","Beef king"};
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
@@ -30,18 +34,38 @@ public class HomeFragment extends Fragment {
     }
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
-
         super.onActivityCreated(savedInstanceState);
-
+        mSearchView = (SearchView)getActivity().findViewById(R.id.searchView);
         listView = (ListView) getActivity().findViewById(R.id.restaurantsListVw);
+        listView.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, restaurantName));
+        listView.setTextFilterEnabled(true);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 TextView name = (TextView)view;
-                Toast.makeText(getActivity(), name.getText()+"", Toast.LENGTH_LONG).show();
+//                Toast.makeText(getActivity(), name.getText()+"", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(getActivity(), RestaurantItemActivity.class);
                 intent.putExtra("RN",name.getText());
                 startActivity(intent);
+            }
+        });
+
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            // This method is triggered when the search button is clicked
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            // This method is triggered when the search content changes
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (!TextUtils.isEmpty(newText)){
+                    listView.setFilterText(newText);
+                }else{
+                    listView.clearTextFilter();
+                }
+                return false;
             }
         });
 
