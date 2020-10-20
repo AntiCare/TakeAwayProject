b0206b53534493b23a67234438789956941dd238
yang Cheng <474340@student.saxion.nl>
2020-10-20 10:57:32 +0200

    message function: send message to firebase

--- TestNav/app/src/main/java/nl/saxion/testnav/MessageActivity.java
+++ TestNav/app/src/main/java/nl/saxion/testnav/MessageActivity.java
@@ -1,78 +1,70 @@
 package nl.saxion.testnav;
 
-import android.accounts.Account;
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import com.bumptech.glide.Glide;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import de.hdodenhof.circleimageview.CircleImageView;
-//import nl.saxion.testnav.models.Account;
+import java.util.HashMap;
+
 
 public class MessageActivity extends AppCompatActivity {
-    private CircleImageView profileImg;
-    private TextView recipientName;
+    ImageButton sendButton;
+    EditText sendText;
 
-    private FirebaseUser fUser;
-    DatabaseReference ref;
-
-    Intent intent;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+
+        initialVariable();
+        event();
+
+
+
+    }
+    public void event() {
+        sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                String msg = sendText.getText().toString();
+                if (!msg.equals("")) {
+                    sendMessage("SENDER","RECEIVER",msg);
+                }else {
+                    Toast.makeText(MessageActivity.this,"Can't send empty message!!!",Toast.LENGTH_SHORT).show();
+                }
+                sendText.setText("");
             }
         });
+    }
 
-        profileImg = findViewById(R.id.recipientProfileImgVw);
-        recipientName = findViewById(R.id.recipientNameTxtVw);
 
-        intent = getIntent();
-        String userid = intent.getStringExtra("userid");
 
-        fUser = FirebaseAuth.getInstance().getCurrentUser();
-        ref = FirebaseDatabase.getInstance().getReference("Users").child(userid);
 
-//        ref.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                Account user = snapshot.getValue(Account.class);
-//                recipientName.setText(user.getFirstName());
-//                if (user.getImageURL().equals("default")) {
-//                    profileImg.setImageResource(R.mipmap.ic_launcher);
-//                } else {
-//                    Glide.with(MessageActivity.this).load(user.getImageURL()).into(profileImg);
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//
-//            }
-//        });
+    public void sendMessage (String sender, String receiver, String message) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender",sender);
+        hashMap.put("receiver",receiver);
+        hashMap.put("message",message);
+
+        reference.child("Chats").push().setValue(hashMap);
+    }
+
+
+    public void initialVariable() {
+        sendButton= findViewById(R.id.btn_send);
+        sendText= findViewById(R.id.text_send);
     }
 }
